698804e7e8958315fb20574dea05ced32c343151
royhung <lemanhhung1555@gmail.com>
2025-09-04 05:08:25 +0700

    state

--- src/App.js
+++ src/App.js
@@ -4,30 +4,39 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 
 import MyComponent from './components/MyComponent';
+import React from 'react';
 
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-
-    <div>
+class App extends React.Component{
+  render(){
+    return(
+      <div>
       Hello world royhung
       <MyComponent></MyComponent>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello world royhung
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+    );
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+
+   
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello world royhung
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
  
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,14 +4,20 @@
 import React from "react";
 
 class MyComponent extends React.Component{
+
+    state = {
+        name: 'Roy hung',
+        address: 'Ha Noi',
+        age: '22',
+
+    };
+
     //JSX: The special syntax of React is JSX, which allows us to write JavaScript inside HTML-like code.
     render(){
         return(
 
             <div>
-                My first component
-
-                {Math.random()}
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         );
     }
